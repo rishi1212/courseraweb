02dca5e2f40281b228c3bc41531bbc326583d983
rishi <me.rishiverma@gmail.com>
2016-07-12 13:16:04 +0530

    rishi:week6 assigments

---
+++ src/jsonPackage/assignment1.py
@@ -0,0 +1,16 @@
+import json
+import urllib
+
+url='http://python-data.dr-chuck.net/comments_298010.json'
+
+data =urllib.urlopen(url).read()
+
+info = json.loads(data)
+comments=info["comments"]
+
+counts=[]
+
+for comment in comments:
+    counts.append(int(comment['count']))
+
+print sum(counts)
